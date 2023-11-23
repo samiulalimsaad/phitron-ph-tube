bfbbdbb90db457fb3984137d64d1b04dca82b412
samiulalimsaad <samiulalimsaad@gmail.com>
2023-11-24 00:29:38 +0600

    show data based on category

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
+let currentCategory = 1000;
 const categoryNode = document.querySelector("#category");
+const categoryDataNode = document.querySelector("#data");
 
 async function getCategories() {
     const res = await fetch(
@@ -21,9 +23,58 @@ async function getDataByCategories(id) {
 
 async function setCategories() {
     const categories = await getCategories();
+    categoryNode.innerHTML = "";
     categories?.map((c) => {
-        categoryNode.innerHTML += `<button id="${c.category_id}" class="btn" onclick="getDataByCategories('${c.category_id}')" >${c.category}</button>`;
+        categoryNode.innerHTML += `
+            <button 
+                id="${c.category_id}" 
+                class="btn ${
+                    c.category_id == currentCategory ? "btn-error" : ""
+                }" 
+                onclick="setCategoriesData('${c.category_id}')" 
+            >
+                ${c.category}
+            </button>
+        `;
+    });
+}
+
+async function setCategoriesData(id = 1000) {
+    currentCategory = id;
+    setCategories();
+    const data = await getDataByCategories(id);
+    categoryDataNode.innerHTML = "";
+    data?.map((c) => {
+        categoryDataNode.innerHTML += `
+        <div id="${c.category_id}" class="card card-compact bg-base-100 shadow-xl">
+                <figure class="h-52">
+                    <img
+                        class="object-cover"
+                        src="${c.thumbnail}"
+                        alt="${c.title}"
+                    />
+                </figure>
+                <div class="card-body">
+                    <div class="flex items-center gap-2">
+                        <div class="avatar">
+                            <div class="w-8 rounded-full">
+                                <img
+                                    src="${c.authors?.[0]?.profile_picture}"
+                                />
+                            </div>
+                        </div>
+
+                        <h2 class="card-title">${c.title}</h2>
+                    </div>
+                    <div class="pl-10">
+                        <h3 class="">${c.authors?.[0]?.profile_name}</h3>
+                        <p class="card-actions">${c.others?.views}</p>
+                    </div>
+                </div>
+            </div>
+        `;
     });
 }
 
 setCategories();
+setCategoriesData();
