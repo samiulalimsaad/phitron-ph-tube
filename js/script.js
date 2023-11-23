e064d682f41b7def0e472cb71580b68b1276859a
samiulalimsaad <samiulalimsaad@gmail.com>
2023-11-24 00:03:43 +0600

    get data by category added

--- js/script.js
+++ js/script.js
@@ -9,10 +9,20 @@ async function getCategories() {
     return data;
 }
 
+async function getDataByCategories(id) {
+    const res = await fetch(
+        `https://openapi.programming-hero.com/api/videos/category/${id}`
+    );
+
+    const { data } = await res.json();
+    console.log(data);
+    return data;
+}
+
 async function setCategories() {
     const categories = await getCategories();
     categories?.map((c) => {
-        categoryNode.innerHTML += `<button id="${c.category_id}" class="btn"  >${c.category}</button>`;
+        categoryNode.innerHTML += `<button id="${c.category_id}" class="btn" onclick="getDataByCategories('${c.category_id}')" >${c.category}</button>`;
     });
 }
 
